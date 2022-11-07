6682ed088a20e46d134dc0541c9ae6ab9654fb17
cauegraciolip <79729432+cauegraciolip@users.noreply.github.com>
2022-11-06 23:45:52 -0300

    Add files via upload

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Ana Tompson",
+      email: "ana@gmail.com",
+      avatarUrl: "https://github.com/cauegraciolip.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example pool",
+      codigo: "BOL126",
+      ownerId: user.id,
+
+      Participant: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T12:00:00.325Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-03T12:00:00.325Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
