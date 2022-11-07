e3387c12c8a2b3e1924d573d54e388effb9e9d4b
cauegraciolip <79729432+cauegraciolip@users.noreply.github.com>
2022-11-06 23:44:23 -0300

    Add files via upload

---
+++ mobile/src/components/Guesses.tsx
@@ -0,0 +1,96 @@
+import { Box, FlatList, useToast } from "native-base";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Game, GameProps } from "./Game";
+import { Loading } from "./Loading";
+
+interface Props {
+  poolId: string;
+}
+
+export function Guesses({ poolId }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [firstTeamPoints, setFirstTeamPoints] = useState("");
+  const [secondTeamPoints, setSecondTeamPoints] = useState("");
+  const [games, setGames] = useState<GameProps[]>([]);
+  const toast = useToast();
+
+  async function fetchGame() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get(`/pools/${poolId}/games`);
+
+      setGames(response.data.games);
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: "Não foi possível carregar os jogos",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      setIsLoading(true);
+      if (!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: "Informe o placar do palpite",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      });
+
+      toast.show({
+        title: "Palpite realizado com sucesso",
+        placement: "top",
+        bgColor: "green.500",
+      });
+
+      fetchGame();
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Não foi possível enviar o palpite",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGame();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <FlatList
+      data={games}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
+        />
+      )}
+      _contentContainerStyle={{ pb: 150 }}
+    />
+  );
+}
---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,29 @@
+import { Center, Icon, Text } from "native-base";
+import { Button } from "../components/Button";
+import { Fontisto } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+import { useAuth } from "../hooks/useAuth";
+
+export function SignIn() {
+  const { signIn, isUserLoading } = useAuth();
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM O GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        type="SECONDARY"
+        mt={10}
+        onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{ _spinner: { color: "white" } }}
+      />
+      <Text color="gray.400" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para a
+        criação de sua conta
+      </Text>
+    </Center>
+  );
+}
